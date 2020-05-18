41bc23ccd9ea1ead481fd6a80f1927f0d4d1a19b
Ricardo Verhaeg <ricardo@arquivei.com.br>
2020-05-18 17:05:09 -0300

    Adicao de variaveis em um arquivo na raiz para ser pego pelo crontab

--- apache/run.sh
+++ apache/run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+echo "Saving env vars for crontab"
+declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
+
 echo "Starting cron and filebeat"
 /etc/init.d/cron restart
 /etc/init.d/filebeat restart
 PIDFILE=`cat /etc/pgbouncer/pgbouncer.ini  | grep pidfile | cut -d ' ' -f 3`
+
 echo "Removing '$PIDFILE' and starting PgBouncer"
 rm -f $PIDFILE
 /usr/sbin/pgbouncer -d -u postgres /etc/pgbouncer/pgbouncer.ini
+
 echo "Starting Apache"
 apache2-foreground
